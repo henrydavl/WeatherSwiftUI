1fa5d5f269a50619a31cc158f01ac3833cd00026
Henry David Lie <32681485+henrydavl@users.noreply.github.com>
2021-12-07 09:43:28 +0700

    initial commit

---
+++ SimpleWeather/Components/WeatherRowView.swift
@@ -0,0 +1,41 @@
+//
+//  WeatherRowView.swift
+//  SimpleWeather
+//
+//  Created by Henry David Lie on 07/12/21.
+//
+
+import SwiftUI
+
+struct WeatherRowView: View {
+    var logo: String
+    var name: String
+    var value: String
+    
+    
+    var body: some View {
+        HStack(spacing: 20) {
+            Image(systemName: logo)
+                .font(.title2)
+                .frame(width: 20, height: 20)
+                .padding()
+                .background(Color(hue: 1.0, saturation: 0.0, brightness: 0.888))
+                .cornerRadius(50)
+            
+            VStack(alignment: .leading, spacing: 8) {
+                Text(name)
+                    .font(.caption)
+                
+                Text(value)
+                    .bold()
+                    .font(.title)
+            }
+        }
+    }
+}
+
+struct WeatherRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        WeatherRowView(logo: "thermometer", name: "Feels like", value: "8°")
+    }
+}
---
+++ SimpleWeather/SimpleWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleWeatherApp.swift
+//  SimpleWeather
+//
+//  Created by Henry David Lie on 07/12/21.
+//
+
+import SwiftUI
+
+@main
+struct SimpleWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
